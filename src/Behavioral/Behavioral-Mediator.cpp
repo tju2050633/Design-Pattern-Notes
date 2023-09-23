5635108de0a828f8cde3e779ab8b8a4cd064ef63
Jialin Lu <1640889387@qq.com>
2023-09-23 13:56:58 +0800

    备忘录、中介者

--- src/Behavioral/Behavioral-Mediator.cpp
+++ src/Behavioral/Behavioral-Mediator.cpp
@@ -3,20 +3,18 @@
 #include <map>
 using namespace std;
 
-/*
-对象间通信
-*/
-
-/* 声明 */
+/* ********************************* */
+// 声明
+/* ********************************* */
 class Mediator;
 class ConcreteMediator;
 class Colleague;
 class ConcreteColleagueA;
 class ConcreteColleagueB;
 
-/* 定义 */
-
-// 中介
+/* ********************************* */
+// 定义
+/* ********************************* */
 class Mediator
 {
 public:
@@ -25,17 +23,6 @@ public:
     virtual void registered(int ID, Colleague *colleague) = 0;
 };
 
-class ConcreteMediator : public Mediator
-{
-private:
-    map<int, Colleague *> colleagues;
-
-public:
-    void send(int receiverID, string message);
-    void registered(int ID, Colleague *colleague);
-};
-
-// 同事
 class Colleague
 {
 protected:
@@ -48,68 +35,64 @@ public:
     virtual void receive(string message) = 0;
 };
 
+class ConcreteMediator : public Mediator
+{
+private:
+    map<int, Colleague *> colleagues;   // 用map来存储同事
+
+public:
+    void send(int receiverID, string message)
+    {
+        if (colleagues.find(receiverID) != colleagues.end())
+        {
+            colleagues[receiverID]->receive(message);
+        }
+        else
+        {
+            cout << "Receiver not found." << endl;
+        }
+    }
+    void registered(int ID, Colleague *colleague)
+    {
+        if (colleagues.find(ID) == colleagues.end())
+        {
+            colleagues[ID] = colleague;
+            colleague->setMediator(this);   // 注册时顺便给同事设置中介
+        }
+        else
+        {
+            cout << "ID already registered." << endl;
+        }
+    }
+};
+
+
+
 class ConcreteColleagueA : public Colleague
 {
 public:
-    void send(int receiverID, string message);
-    void receive(string message);
+    void send(int receiverID, string message)
+    {
+        mediator->send(receiverID, message);
+    }
+    void receive(string message)
+    {
+        cout << "ConcreteColleagueA received: " << message << endl;
+    }
 };
 
 class ConcreteColleagueB : public Colleague
 {
 public:
-    void send(int receiverID, string message);
-    void receive(string message);
+    void send(int receiverID, string message)
+    {
+        mediator->send(receiverID, message);
+    }
+    void receive(string message)
+    {
+        cout << "ConcreteColleagueB received: " << message << endl;
+    }
 };
-
-/* 实现 */
-
-// 中介
-void ConcreteMediator::send(int receiverID, string message)
-{
-    if (colleagues.find(receiverID) != colleagues.end())
-    {
-        colleagues[receiverID]->receive(message);
-    }
-    else
-    {
-        cout << "Receiver not found." << endl;
-    }
-}
-
-void ConcreteMediator::registered(int ID, Colleague *colleague)
-{
-    if (colleagues.find(ID) == colleagues.end())
-    {
-        colleagues[ID] = colleague;
-        colleague->setMediator(this);   // 注册时顺便给同事设置中介
-    }
-    else
-    {
-        cout << "ID already registered." << endl;
-    }
-}
-
-// 同事
-void ConcreteColleagueA::send(int receiverID, string message)
-{
-    mediator->send(receiverID, message);
-}
-
-void ConcreteColleagueA::receive(string message)
-{
-    cout << "ConcreteColleagueA received: " << message << endl;
-}
-
-void ConcreteColleagueB::send(int receiverID, string message)
-{
-    mediator->send(receiverID, message);
-}
-
-void ConcreteColleagueB::receive(string message)
-{
-    cout << "ConcreteColleagueB received: " << message << endl;
-}
 
 int main()
 {
---
+++ src/Behavioral/Behavioral-Memento.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/* ********************************* */
+// 声明
+/* ********************************* */
+
+class Editor;
+class Snapshot;
+class Command;
+
+/* ********************************* */
+// 定义
+/* ********************************* */
+class Editor {
+  private:
+    string text;
+    int curX, curY;
+
+  public:
+    string getText() { return text; }
+    void setText(string text) { this->text = text; }
+
+    int getCursorX() { return curX; }
+    int getCursorY() { return curY; }
+    void setCursor(int x, int y) {
+        this->curX = x;
+        this->curY = y;
+    }
+
+    Snapshot createSnapshot();
+};
+
+class Snapshot {
+  private:
+    Editor *editor;
+    string text;
+    int curX, curY;
+
+  public:
+    Snapshot(Editor *editor, string text, int curX, int curY) {
+        this->editor = editor;
+        this->text = text;
+        this->curX = curX;
+        this->curY = curY;
+    }
+
+    void restore() {
+        editor->setText(text);
+        editor->setCursor(curX, curY);
+    }
+};
+
+Snapshot Editor::createSnapshot(){ return Snapshot(this, text, curX, curY); }
+
+int main() {
+    Editor editor;
+    editor.setText("Hello, world!\n");
+    Snapshot snapshot = editor.createSnapshot();
+
+    editor.setText("This is a test.\n");
+    snapshot.restore();
+    cout << editor.getText();
+
+    return 0;
+}
